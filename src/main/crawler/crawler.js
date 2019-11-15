d34f2283dc67fdb4317dbe531087280a7673fa23
YomeR7 <33492316+YomeR7@users.noreply.github.com>
2019-11-15 16:56:55 -0500

    update crawler

--- src/main/crawler/crawler.js
+++ src/main/crawler/crawler.js
@@ -2,49 +2,55 @@ var request = require('request');
 var cheerio = require('cheerio');
 
 //scrolling de toutes les pages de sort
-function crawl() {
+function crawlLien(callback = null) {
     console.log('CRAWLING...');
     //url du site a scroller
-    var url = "http://legacy.aonprd.com/indices/bestiary.html";
-    var crawlResult = new Array();
-var urlArray=new Array();
+    var url = "http://legacy.aonprd.com/indices/bestiary.html"
 
-        request(url, function (error, response, body) {
-            if (!error) {
-                var $ = cheerio.load(body); //on recupere le body de la page html
+    var urlArray = new Array();
 
-
-                var urlsMonsters = $('div.index li').map(function (i, el) {
-                    // this === el
-                    return $(this).html();
-                }).get().join('\n');
-
-console.log(urlsMonsters);
-
-                    var JSONobj = {
-
-                    };
-                    console.log(JSONobj);
-                    crawlResult.push(JSONobj);
+    request(url, function (error, response, body) {
+        if (!error) {
+            var $ = cheerio.load(body); //on recupere le body de la page html
 
 
-                }
+            var urlsMonsters = $('div.index li').map(function (i, el) {
+                // this === el
+                return $(this).html();
+            }).get().join('\n');
+            urlArray = urlsMonsters.match(/\/{1}.*html/g);
+            console.log(urlsMonsters);
+            console.log(urlArray);
 
+            /*   var JSONobj = {
+  
+              };
+              console.log(JSONobj);
+              crawlResult.push(JSONobj); */
+            if (callback != null) callback(urlArray)
 
-
-
-            else {
-                console.log("We’ve encountered an error: " + error);
-            }
-        });
-
-
-
-    return crawlResult; //on retourne le tableau
-
+        } else {
+            console.log("We’ve encountered an error: " + error);
+        }
+    });
 }
 
-crawl();
+function crawl() {
+    var urlCrawl = "http://legacy.aonprd.com/indices";
+    var crawlResult = new Array();
+    crawlLien((urlArray) => {
+        for (var i = 0; i < urlArray.length; i++) {
+            url= urlCrawl+urlArray[i];
+            console.log(url)
+            request(url, function (error, response, body) {
+                if (!error) {
+                    var $ = cheerio.load(body); //on recupere le body de la page html de chaque monstre
+                    
+                }
+            });
+        }
+    });
+}
 
 
-
+crawl()
