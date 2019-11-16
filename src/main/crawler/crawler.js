382ad377356ca33f386bfa85e64489ac172a12e0
charlenelecouturier <33778377+charlenelecouturier@users.noreply.github.com>
2019-11-16 09:03:54 -0500

    feature : crawler
    on a le JSON avec les datas

--- src/main/crawler/crawler.js
+++ src/main/crawler/crawler.js
@@ -1,7 +1,17 @@
 var request = require('request');
 var cheerio = require('cheerio');
 
-//scrolling de toutes les pages de sort
+
+const fs = require('fs')
+
+function EcrireJson(jsonObj) {
+
+
+
+    let donnees = JSON.stringify(jsonObj)
+    fs.writeFileSync('dataset.json', donnees)
+}
+
 function crawlLien(callback = null) {
     console.log('CRAWLING...');
     //url du site a scroller
@@ -19,14 +29,10 @@ function crawlLien(callback = null) {
                 return $(this).html();
             }).get().join('\n');
             urlArray = urlsMonsters.match(/\/{1}.*html/g);
-            console.log(urlsMonsters);
-            console.log(urlArray);
+            //  console.log(urlsMonsters);
+            //  console.log(urlArray);
 
-            /*   var JSONobj = {
-  
-              };
-              console.log(JSONobj);
-              crawlResult.push(JSONobj); */
+
             if (callback != null) callback(urlArray)
 
         } else {
@@ -36,21 +42,80 @@ function crawlLien(callback = null) {
 }
 
 function crawl() {
-    var urlCrawl = "http://legacy.aonprd.com/indices";
+    var urlCrawl = "http://legacy.aonprd.com";
     var crawlResult = new Array();
     crawlLien((urlArray) => {
+
+        //on crawl toutes les urls des monstres pour avoir leurs sorts
         for (var i = 0; i < urlArray.length; i++) {
-            url= urlCrawl+urlArray[i];
+            url = urlCrawl + urlArray[i];
             console.log(url)
             request(url, function (error, response, body) {
                 if (!error) {
                     var $ = cheerio.load(body); //on recupere le body de la page html de chaque monstre
-                    
+
+                    var name = $("title").text();
+
+                    //   console.log(name);
+                    var spells = $('body p').map(function (i, el) {
+                        // this === el
+                        return $(this).html();
+                    }).get().join('\n');
+                    //console.log(spells)
+
+                    var spellsArray = spells.match(/\/spells\/.*?<\/a>/g) //on récupère les sorts
+                    if (spellsArray != null) {
+                        spellsArray = removeDuplicates(spellsArray);
+                        for (var i = 0; i < spellsArray.length; i++) {
+
+                            //on decoupe la chaine de caratères pour n'avoir que le nom du spell
+                            var debut = spellsArray[i].indexOf(">") + 1;
+                            spellsArray[i] = spellsArray[i].slice(debut, -4);
+                            if (spellsArray[i].indexOf("<em>") != -1) {
+                                spellsArray[i] = spellsArray[i].slice(4, -5);
+
+                            }
+
+                            if (spellsArray[i].indexOf("<i>") != -1) {
+                                spellsArray[i] = spellsArray[i].slice(3, -4);
+                            }
+
+                        }
+                        //  console.log(spellsArray);
+                    } var JSONobj = {
+                        name: name,
+                        spells: spellsArray
+                    };
+                    // console.log(JSONobj);
+                    crawlResult.push(JSONobj);
+                } else {
+
+                    console.log("We’ve encountered an error: " + error);
                 }
             });
         }
     });
+
+    return crawlResult;
 }
 
 
-crawl()
+
+var result = crawl();
+
+setTimeout(function () {
+    EcrireJson(result);
+
+}, 300000); //on laisse un delai de avant d'ecrire dans un fichier json pour que le crawler ait fini son execution
+
+
+function removeDuplicates(tab) {
+    let unique = {};
+    tab.forEach(function (i) {
+        if (!unique[i]) {
+            unique[i] = true;
+        }
+    });
+    return Object.keys(unique);
+}
+
