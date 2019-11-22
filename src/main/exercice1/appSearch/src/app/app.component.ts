54fcedb764d95d8f2d6a5a747bdafa5e2caf9714
charlenelecouturier <33778377+charlenelecouturier@users.noreply.github.com>
2019-11-22 14:46:03 -0500

    update form

--- src/main/exercice1/appSearch/src/app/app.component.ts
+++ src/main/exercice1/appSearch/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormArray,FormControl } from '@angular/forms';
 import { Alert } from 'selenium-webdriver';
 import { CommonService } from './common.service';
 
@@ -12,21 +12,87 @@ export class AppComponent implements OnInit{
   title = 'appSearch';
   
 form;
-
+componentsdata;
+classdata;
   constructor(private formBuilder: FormBuilder,private newService :CommonService
     ) { 
-    
+    this.componentsdata=[
+      { id: 'V', name: 'Vocal' },
+      { id:'S', name: 'Somatic' },
+      { id: 'M', name: 'Material' },
+    ];
+
+    this.classdata=[
+      { id: 'sorcerer/wizard', name: 'sorcerer/wizard' },
+      { id:'ranger', name: 'ranger' },
+      { id: 'summoner', name: 'summoner' },
+      { id: 'inquisitor', name: 'inquisitor' },
+      { id:'antipaladin', name: 'antipaladin' },
+      { id: 'cleric/oracle', name: 'cleric/oracle' },
+      { id: 'shaman', name: 'shaman' },
+      { id:'alchemist', name: 'alchemist' },
+      { id: 'witch', name: 'witch' },
+      { id: 'druid', name: 'druid' },
+      { id:'paladin', name: 'paladin' },
+      { id: 'bard', name: 'bard' },
+      { id:'magus', name: 'magus' },
+      { id: 'bloodrager', name: 'bloodrager' },
+    ];
 
     this.form = this.formBuilder.group({
       mots:'',
+      level:'',
+      component: new FormArray([]),
+      class: new FormArray([]),
+    
+    
+    });
+
+    this.addCheckboxes();
+  }
+
+
+
+  private addCheckboxes() {
+    this.componentsdata.forEach((o, i) => {
+      const control = new FormControl(false); // if first item set to true, else false
+      (this.form.controls.component as FormArray).push(control);
+    });
+
+
+    this.classdata.forEach((o, i) => {
+      const control = new FormControl(false); // if first item set to true, else false
+      (this.form.controls.class as FormArray).push(control);
     });
   }
 
+
+
   onSubmit(data) {
-   alert(data.mots);
-   this.newService.getSpellsbyKeyWords(data.mots);
-    this.form.reset();
+
+   this.newService.getSpellsbyKeyWords(data);
+
+
+    const selectedComponentsIds = data.component
+    .map((v, i) =>v? this.componentsdata[i].id  : null)
+    .filter(v => v !== null);
+
+    
+    const classIds = data.class
+    .map((v, i) =>v? this.classdata[i].id  : null)
+    .filter(v => v !== null);
+    data.class=classIds;
+    data.component=selectedComponentsIds;
+    this.newService.getSpellsbyKeyWords(data);
+
+  alert(data.component);
+  this.form.reset();
+
+
   }
+
+
+  
 
   ngOnInit() {
   }
